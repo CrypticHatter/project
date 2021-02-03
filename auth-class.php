98699a2c8eb15d2a083b86076fc04abc15328ecb
Rajitha Fernando <chandimalfdo23@gmail.com>
2021-02-03 15:03:07 +0530

    start

--- auth-class.php
+++ auth-class.php
@@ -1,11 +1,11 @@
 <?php
-class AuthClass{
-    public static function signup(){
+include_once('db.php');
 
+class AuthClass extends Db{
+    public static function signup($email, $password){
     }
 
-    public static function login(){
-        
+    public static function logon($email, $password){
     }
 
     public static function logout(){
--- db.php
+++ db.php
@@ -1,26 +1,9 @@
 <?php
 class Db{
-    private $host;
-    private $username;
-    private $password;
-    private $db;
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $db = 'project';
 
     protected $connection;
-
-    public function __construct(){
-        if (!isset($this->connection)) {
-            $this->host = 'localhost';
-            $this->username= 'root';
-            $this->password= '';
-            $this->db= 'project';
-
-            $this->connection = new mysqli($this->host, $this->username, $this->password, $this->db);
- 
-            if (!$this->connection) {
-                echo 'Cannot connect to database server';
-                exit;
-            }            
-        }
-        return $this->connection;
-    }
 }
