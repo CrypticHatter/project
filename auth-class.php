92ff73c74d026da34fda30fc1fef0de62461820a
Rajitha Fernando <chandimalfdo23@gmail.com>
2021-02-03 12:49:29 +0530

    first commit

---
+++ auth-class.php
@@ -0,0 +1,14 @@
+<?php
+class AuthClass{
+    public static function signup(){
+
+    }
+
+    public static function login(){
+        
+    }
+
+    public static function logout(){
+        
+    }
+}
